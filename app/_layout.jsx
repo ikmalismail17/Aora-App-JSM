1bfc7960bb4519cf75a3221337f721827e009447
ikmalismail17 <ikmal5a2013@gmail.com>
2024-08-12 20:57:45 +0800

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
 import React, { useEffect } from 'react'
+import GlobalProvider from '../context/GlobalProvider';
 
 // prevent the splash screen from auto hiding before assets loading is complete
 SplashScreen.preventAutoHideAsync();
@@ -31,12 +32,14 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
 
   return ( 
+    <GlobalProvider>
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
       <Stack.Screen name='(auth)' options={{ headerShown: false }} />
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
       {/* <Stack.Screen name='/search/[query]' options={{ headerShown: false }} /> */}
     </Stack>
+    </GlobalProvider>
   )
 }
 
